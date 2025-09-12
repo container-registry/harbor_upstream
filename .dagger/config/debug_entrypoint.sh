9325165ce9d24181035d96db7f8d22c6878687f8
bupd <bupdprasanth@gmail.com>
2025-09-13 03:22:21 +0530

    uncommit(ci): fix debug entrypoint in dagger
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/config/debug_entrypoint.sh
+++ .dagger/config/debug_entrypoint.sh
@@ -6,7 +6,7 @@ echo "Executing:dlv --headless=true --allow-non-terminal-interactive=true --disa
 # Start the dlv process in the background
 # /root/go/bin/dlv exec --headless --listen localhost:$2 $1
 # connect to your local dap server (for dap debugging)
-dlv dap --client-addr=192.168.0.4:4001 --log
+# dlv dap --client-addr=192.168.0.4:4001 --log
 # use below to normal debug session
-# dlv --headless=true --allow-non-terminal-interactive=true --disable-aslr=true --listen=0.0.0.0:4001 --accept-multiclient=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --log=true --continue --api-version=2 exec $1
+dlv --headless=true --allow-non-terminal-interactive=true --disable-aslr=true --listen=0.0.0.0:4001 --accept-multiclient=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --log=true --continue --api-version=2 exec $1
 # dlv --headless=true --listen=0.0.0.0:4001 --accept-multiclient --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --log=true --api-version=2 attach $pid
