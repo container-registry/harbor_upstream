40a907e8a9039e66005d0a76180272edc138dc88
bupd <bupdprasanth@gmail.com>
2025-09-28 23:29:24 +0530

    feat: add fidp ctl consts model
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ src/controller/federatedidp/model.go
@@ -0,0 +1,11 @@
+package federated_idp
+
+const (
+	// LEVELSYSTEM ...
+	LEVELSYSTEM = "system"
+	// LEVELPROJECT ...
+	LEVELPROJECT = "project"
+
+	// SCOPEALLPROJECT ...
+	SCOPEALLPROJECT = "/project/*"
+)
