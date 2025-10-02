5bb330aec7c35b196c52c77ed13a87602bbf42c5
bupd <bupdprasanth@gmail.com>
2025-10-03 04:37:08 +0530

    feat: update agains
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/server/middleware/security/robotjwt.go
+++ src/server/middleware/security/robotjwt.go
@@ -166,35 +166,35 @@ func (r *robotjwt) Generate(req *http.Request) security.Context {
 	log.Warningf("the sign method is %v", signMethod)
 
 	// convert jwk in bytes and return a new key
-	jwkeySet, err := jwk.Parse([]byte(jwkskeys))
-	if err != nil {
-		fmt.Printf("failed to parse key: %s\n", err)
-		return nil
-	}
+	// jwkeySet, err := jwk.Parse([]byte(jwkskeys))
+	// if err != nil {
+	// 	fmt.Printf("failed to parse key: %s\n", err)
+	// 	return nil
+	// }
+	//
+	// jwKey, ok := jwkeySet.LookupKeyID(kid)
+	// if !ok {
+	// 	fmt.Printf("failed to find key with kid: %s\n", kid)
+	// 	return nil
+	// }
 
-	jwKey, ok := jwkeySet.LookupKeyID(kid)
-	if !ok {
-		fmt.Printf("failed to find key with kid: %s\n", kid)
-		return nil
-	}
-
-	jwkBytes1, err := jwk.EncodePEM(jwkKey)
-	if err != nil {
-		log.Warningf("failed to convert JWK to public key bytes: %v", err)
-	}
-
-	jwkBytes2, err := jwk.EncodePEM(jwKey)
+	jwkBytes, err := jwk.EncodePEM(jwkKey)
 	if err != nil {
 		log.Warningf("failed to convert JWK to public key bytes: %v", err)
 	}
 
-	log.Warningf("the jwk bytes1 are %v", jwkBytes1)
-	log.Warningf("the jwk bytes2 are %v", jwkBytes2)
+	// jwkBytes2, err := jwk.EncodePEM(jwKey)
+	// if err != nil {
+	// 	log.Warningf("failed to convert JWK to public key bytes: %v", err)
+	// }
+
+	log.Warningf("the jwk bytes1 are %v", jwkBytes)
+	// log.Warningf("the jwk bytes2 are %v", jwkBytes2)
 
 	// TODO: remove hardcoded to RS256
 	defaultOpt.Issuer = issuer
-	defaultOpt.PrivateKey = []byte("")
-	defaultOpt.PublicKey = jwkBytes1
+	defaultOpt.PrivateKey = nil
+	defaultOpt.PublicKey = jwkBytes
 
 	// token.parse will just check the validity of the token and parse the token, validating the given claims
 	t, err := token.Parse(defaultOpt, tokenStr, cl)
