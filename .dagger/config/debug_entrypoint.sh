6eaf027880ae264c71fa5c5a0b849a92eea67df8
bupd <bupdprasanth@gmail.com>
2025-10-03 03:15:59 +0530

    feat: update debug entrypoint
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/config/debug_entrypoint.sh
+++ .dagger/config/debug_entrypoint.sh
@@ -9,7 +9,7 @@ echo "Executing: dlv dap --listen=:4001 --api-version=2 exec $1"
 # connect to your local dap server (for dap debugging)
 # dlv dap --client-addr=192.168.0.4:4001 --log
 # use below to normal debug session
-dlv dap --listen=:4001 --api-version=2 --log --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc,dap,fncall,minidump,stack exec $1
+dlv dap --listen=:4001 --api-version=2 --log --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc exec $1
 
 # dlv --headless=true --allow-non-terminal-interactive=true --disable-aslr=true --listen=0.0.0.0:4001 --accept-multiclient=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --log=true --continue --api-version=2 exec $1
 # dlv --headless=true --listen=0.0.0.0:4001 --accept-multiclient --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --log=true --api-version=2 attach $pid
