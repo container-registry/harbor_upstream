0c1cb91c0fd2df4b62d2a5a574741598b1ee0754
bupd <bupdprasanth@gmail.com>
2025-10-03 13:30:43 +0530

    feat: add jwthandler
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ src/server/middleware/security/jwthandler/jwthandler.go
@@ -0,0 +1,22 @@
+package jwthandler
+
+import (
+	"log"
+
+	"github.com/goharbor/harbor/src/common"
+	"github.com/lestrrat-go/jwx/v3/jwk"
+	"github.com/lestrrat-go/jwx/v3/jwt"
+)
+
+func ParseToken(token string, jwkSet jwk.Set) (jwt.Token, error) {
+	// parse the token
+	parsedToken, err := jwt.Parse([]byte(token), jwt.WithKeySet(jwkSet), jwt.WithValidate(true), jwt.WithAcceptableSkew(common.JwtLeeway))
+	if err != nil {
+		log.Fatalf("\n\nFailed to verify JWT signature: %v", err)
+	}
+
+	log.Println("\n\nJWT signature validation successful!")
+	log.Printf("Parsed Token: %+v\n", parsedToken)
+
+	return parsedToken, nil
+}
