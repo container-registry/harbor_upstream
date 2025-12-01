0a8d573969dd02f4bc66fd21e666fce19324f7fd
bupd <bupdprasanth@gmail.com>
2025-12-01 15:36:52 +0530

    feat: remove unused migration
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- make/migrations/postgresql/0171_8gears_cr_schema.up.sql
+++
@@ -1,4 +0,0 @@
-/*
-Add new column single_active_replication for replication_policy table to support single_active_replication
-*/
-ALTER TABLE replication_policy ADD COLUMN IF NOT EXISTS single_active_replication boolean;
